7f973e39436173ad9ae18c9b059decf985eb43e6
Sam <sam@developersam.com>
2018-05-08 00:04:11 -0400

    Restructure data class

---
+++ jvm/src/main/kotlin/com/developersam/api/cornell/Test.java
@@ -0,0 +1,9 @@
+package com.developersam.api.cornell;
+
+public final class Test {
+    
+    public static void main(String... args) {
+    
+    }
+    
+}
--- jvm/src/main/kotlin/com/developersam/api/cornell/data/Rosters.kt
+++ jvm/src/main/kotlin/com/developersam/api/cornell/data/Rosters.kt
@@ -5,8 +5,12 @@ import com.google.gson.annotations.SerializedName
 /**
  * [RostersResponse] is the response of a Rosters request.
  */
-internal class RostersResponse private constructor(private val data: Data) {
+internal class RostersResponse private constructor() {
 
+    /**
+     * Main data of response.
+     */
+    private lateinit var data: Data
     /**
      * Obtain the rosters information.
      */
@@ -16,35 +20,88 @@ internal class RostersResponse private constructor(private val data: Data) {
     /**
      * [Data] is an uninteresting class that just holds an array of [Roster].
      */
-    private inner class Data private constructor(val rosters: List<Roster>)
+    private inner class Data private constructor() {
+        lateinit var rosters: List<Roster>
+    }
 
-}
-
-private fun yesNoToBoolean(yesNo: String) = when (yesNo) {
-    "Y" -> true
-    "N" -> false
-    else -> throw RuntimeException("Illegal value!")
 }
 
 /**
  * [Roster] contains all the useful information about Cornell Roster.
  */
-class Roster private constructor(
-        @SerializedName(value = "slug")
-        val semester: String,
-        val isDefaultRoster: Bool,
-        @SerializedName(value = "strm")
-        val semesterCode: String,
-        @SerializedName(value = "descr")
-        val description: String,
-        @SerializedName(value = "descrshort")
-        val shortDescription: String,
-        val defaultSessionCode: String,
-        val defaultCampus: String,
-        val defaultLocation: String,
-        val defaultInstructionMode: String,
-        val sharing: Bool,
-        @SerializedName(value = "archiveMode")
-        val inArchiveMode: Bool,
-        val lastModifiedDttm: String
-)
+class Roster private constructor() {
+
+    /**
+     * Semester of the roster.
+     */
+    @SerializedName(value = "slug")
+    lateinit var semester: String
+        private set
+
+    /**
+     * Whether the roster is the default one.
+     */
+    val isDefaultRoster: Bool = Bool.N
+
+    /**
+     * Code of the semester.
+     */
+    @SerializedName(value = "strm")
+    lateinit var semesterCode: String
+        private set
+
+    /**
+     * A description of the semester.
+     */
+    @SerializedName(value = "descr")
+    lateinit var description: String
+        private set
+
+    /**
+     * A short description of the semester.
+     */
+    @SerializedName(value = "descrshort")
+    lateinit var shortDescription: String
+        private set
+
+    /**
+     * The default session code.
+     */
+    lateinit var defaultSessionCode: String
+        private set
+
+    /**
+     * The default campus.
+     */
+    lateinit var defaultCampus: String
+        private set
+
+    /**
+     * The default location.
+     */
+    lateinit var defaultLocation: String
+        private set
+
+    /**
+     * The default instruction mode.
+     */
+    lateinit var defaultInstructionMode: String
+        private set
+
+    /**
+     * Whether you can share
+     */
+    val sharing: Bool = Bool.N
+
+    /**
+     * Whether the semester's info is in archieve move.
+     */
+    @SerializedName(value = "archiveMode")
+    val isInArchiveMode: Bool = Bool.N
+
+    /**
+     * A datetime stamp of the last modified date.
+     */
+    lateinit var lastModifiedDttm: String
+        private set
+}
